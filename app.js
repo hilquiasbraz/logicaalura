70b0edcc679f28cf3d36bd37d5d6966272eccc36
Hilquias de Sousa Braz <hlqsss8@gmail.com>
2025-08-08 10:09:33 -0300

    Adição de tentativas

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ let numeroSecreto = 5;
 console.log("Valor do número secreto:", numeroSecreto);
 let chute;
 console.log("Valor do chute:", chute);
+let tentativas = 1;
 
 while (chute != numeroSecreto) {
     chute = prompt("Escolha um número entre 1 e 10");
     if (chute == numeroSecreto) {
-        alert(`Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -15,4 +16,6 @@ while (chute != numeroSecreto) {
             alert(`O número secreto é maior que ${chute}`);
         }
     }
+    // tentativas = tentativas + 1;
+    tentativas++;
 }
