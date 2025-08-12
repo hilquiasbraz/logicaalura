9507019753602cfb63dea5415d39805f41799fb1
Hilquias de Sousa Braz <hlqsss8@gmail.com>
2025-08-12 16:16:15 -0300

    Boas práticas de programação

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 alert("Boas vindas!");
-let numeroSecreto = 5;
+// function getRandomInt(min, max) {
+//     min = Math.ceil(100);
+//     max = Math.floor(0)
+// }
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log("Valor do número secreto:", numeroSecreto);
 let chute;
 console.log("Valor do chute:", chute);
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
-    chute = prompt("Escolha um número entre 1 e 10");
+    chute = prompt("Escolha um número entre 1 e 100");
     if (chute == numeroSecreto) {
-        alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -16,6 +20,14 @@ while (chute != numeroSecreto) {
             alert(`O número secreto é maior que ${chute}`);
         }
     }
-    // tentativas = tentativas + 1;
     tentativas++;
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`O número secreto é ${numeroSecreto} e você descobriu com ${tentativas} ${palavraTentativa}`);
+
+// if (tentativas > 1) {
+//     alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+// } else {
+//     alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`)
+// }  
