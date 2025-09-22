a512ef9242a341f18821c991e71152210e11d5d1
Hilquias Braz <hlqsss8@gmail.com>
2025-09-21 22:30:51 -0300

    Adição da lmpeza de campo e habilitação do botão 'Novo Jogo'

--- Lógica 2 - Explore funções e listas/app.js
+++ Lógica 2 - Explore funções e listas/app.js
@@ -17,6 +17,7 @@ function verificarChute() {
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
@@ -24,13 +25,15 @@ function verificarChute() {
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
         tentativas++
+        limparCampo();
     }
-}
-
-function novoJogo() {
-    console.log('O botão foi clicado!');
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
 }
